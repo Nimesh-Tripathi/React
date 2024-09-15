b9dabf1c4ae50f3e7caa8cccc10d65fb1f56b0f6
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-09-15 10:20:59 +0530

    Updated

--- 02-React/App.js
+++ 02-React/App.js
@@ -3,10 +3,44 @@ import ReactDOM from "react-dom/client";
 
 console.log("hello");
 
-const heading = React.createElement("h1",{},"Hello React parcel");
+// React Element 
+const heading = React.createElement("h1", {}, "Hello React parcel");
+
+console.log(heading);
+
+const jsxHeading = <h1>Hello React using jsx</h1>
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+//React component = class based component(old) , functional based component(new)
+
+// React Functional component
+const Heading = () => {
+    return <h1>Hello this is functional react component</h1>
+}
+const Heading2 = () => {
+    return <div>
+        <h1>Hello this is functional react component</h1>
+    </div>
+}
+
+
+// Nesting react component or component composition
+const Para = () => <p>nesting react component</p>
+
+const Hero = () => {
+    return <div>
+        <Para></Para>
+        <div>Heloooooooooooooo</div>
+    </div>
+}
+
+
+// root.render(heading);
+// root.render(jsxHeading);
+// root.render(<Heading></Heading>);
+root.render(<Hero></Hero>)
 
 
