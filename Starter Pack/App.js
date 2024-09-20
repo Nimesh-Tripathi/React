541e8d5a207ea7b621657dd95a3617b9d8e80e96
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-09-20 07:53:27 +0530

    updated Starter pack

--- Starter Pack/App.js
+++ Starter Pack/App.js
@@ -1,4 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useEffect, useState } from "react";
+import Header from "./Component/Header";
+import Body from "./Component/Body";
+import Footer from "./Component/Footer";
 
 
+const AppLayout = () => {
+    return (
+        <div className="App">
+            <Header></Header>
+            <Body></Body>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout></AppLayout>)
